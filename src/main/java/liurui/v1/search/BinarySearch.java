10e96eeedecdb832e0822af66a610f3d56bb8933
liurui <liurui@lingyi365.com>
2017-08-17 08:26:55 +0800

    整理代码

--- src/main/java/liurui/v1/search/BinarySearch.java
+++ src/main/java/liurui/v1/search/BinarySearch.java
@@ -4,16 +4,16 @@ public class BinarySearch {
     public int find(int[] data, int item) {
         int begin = 0;
         int end = data.length - 1;
-        int cur = 0;
+        int cur;
 
         while (begin < end) {
-                cur =  begin +  (end - begin) / 2;
+            cur = begin + (end - begin) / 2;
             if (data[cur] == item)
                 return cur;
             else if (data[cur] > item)
-                end = cur-1;
+                end = cur - 1;
             else
-                begin = cur+1;
+                begin = cur + 1;
         }
         return -1;
     }
