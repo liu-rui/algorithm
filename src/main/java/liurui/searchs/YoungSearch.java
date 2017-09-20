ebce14e2ff0f8412b788e40dbe172e01ce975114
ren <1313475@qq.com>
2017-09-20 21:54:24 +0800

    实现定位查找

---
+++ src/main/java/liurui/searchs/YoungSearch.java
@@ -0,0 +1,13 @@
+package liurui.searchs;
+
+/***
+ * 定位查找杨氏矩阵
+ */
+public class YoungSearch {
+    public YoungSearch(int[][] data) {
+    }
+
+    public boolean search(int item) {
+        return false;
+    }
+}
